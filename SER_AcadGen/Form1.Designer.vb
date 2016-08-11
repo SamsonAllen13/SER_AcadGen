cb6a66754110ae3726bec41d960dd4ab849b69d7
Steven Runck <steven.runck@isa-engineering.com>
2016-08-11 16:51:42 -0400

    First Commit

---
+++ SER_AcadGen/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtTemplate = New System.Windows.Forms.TextBox()
+        Me.lblTemplate = New System.Windows.Forms.Label()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.txtOutfile = New System.Windows.Forms.TextBox()
+        Me.lblOutfile = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtTemplate
+        '
+        Me.txtTemplate.Location = New System.Drawing.Point(163, 124)
+        Me.txtTemplate.Name = "txtTemplate"
+        Me.txtTemplate.Size = New System.Drawing.Size(498, 26)
+        Me.txtTemplate.TabIndex = 0
+        '
+        'lblTemplate
+        '
+        Me.lblTemplate.AutoSize = True
+        Me.lblTemplate.Location = New System.Drawing.Point(82, 127)
+        Me.lblTemplate.Name = "lblTemplate"
+        Me.lblTemplate.Size = New System.Drawing.Size(75, 20)
+        Me.lblTemplate.TabIndex = 1
+        Me.lblTemplate.Text = "Template"
+        '
+        'btnRun
+        '
+        Me.btnRun.Location = New System.Drawing.Point(337, 329)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(77, 33)
+        Me.btnRun.TabIndex = 2
+        Me.btnRun.Text = "Run"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'txtOutfile
+        '
+        Me.txtOutfile.Location = New System.Drawing.Point(163, 179)
+        Me.txtOutfile.Name = "txtOutfile"
+        Me.txtOutfile.Size = New System.Drawing.Size(498, 26)
+        Me.txtOutfile.TabIndex = 3
+        '
+        'lblOutfile
+        '
+        Me.lblOutfile.AutoSize = True
+        Me.lblOutfile.Location = New System.Drawing.Point(82, 185)
+        Me.lblOutfile.Name = "lblOutfile"
+        Me.lblOutfile.Size = New System.Drawing.Size(55, 20)
+        Me.lblOutfile.TabIndex = 4
+        Me.lblOutfile.Text = "Outfile"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(748, 399)
+        Me.Controls.Add(Me.lblOutfile)
+        Me.Controls.Add(Me.txtOutfile)
+        Me.Controls.Add(Me.btnRun)
+        Me.Controls.Add(Me.lblTemplate)
+        Me.Controls.Add(Me.txtTemplate)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtTemplate As TextBox
+    Friend WithEvents lblTemplate As Label
+    Friend WithEvents btnRun As Button
+    Friend WithEvents txtOutfile As TextBox
+    Friend WithEvents lblOutfile As Label
+End Class
---
+++ SER_AcadGen/Form1.vb
@@ -0,0 +1,90 @@
+﻿'Imports necessary libraries
+Imports System.IO
+Imports System.String
+
+Public Class Form1
+    'Runs whenever the run button is clicked
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+        'The locatin of the Template being used;
+        Dim TEMPLATE_NAME As String = txtTemplate.Text
+        'The name of the processed .dxf file;
+        Dim OUTFILE_NAME As String
+        'The current line of text being read from the .dxf file;
+        Dim TextLine As String = ""
+
+        'Holds the rows of data
+        Dim dabaRow As DataRow
+        'Holds the opened database
+        Dim dabaData As New DataSet()
+        'Index variable for assigning data base info into VB arrays
+        Dim ArrayIndex As Integer
+
+        'declare an OleDb connection to a database file using Access MDB file
+        Dim dabaConn As New OleDb.OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\SER_AcadGen\Input\SERmechDB.mdb;User ID=admin;Password=;")
+
+        'Sets up the query and selects the DB table being used 
+        Dim dabaAdap As New OleDb.OleDbDataAdapter("SELECT * FROM TABLENAME", dabaConn)
+
+        'Declares the BugStatus bit
+        Dim DebugActive As Boolean = False
+
+        'Catches a windows error and displays it locally to prevent the program from crashing
+        Try
+            'fills dabaData with the query results
+            dabaAdap.Fill(dabaData, "dabaConn")
+            Console.WriteLine()
+            'A check to prevent crashing if the query did not return any records
+            If dabaData.Tables(0).Rows.Count > 0 Then
+
+                'Loop through the rows in the DB
+                For Each dabaRow In dabaData.Tables(0).Rows
+                    'Set the outfile's name to the text in each row's OUTFILE column and place the file the output folder as a .dxf
+                    OUTFILE_NAME = "C:\SER_AcadGen\Output\" & dabaRow("OUTFILE") & ".dxf"
+                    'Displays the outfile name on the form
+                    txtOutfile.Text = OUTFILE_NAME
+                    'A check to prevent the program from crashing if the template is not present
+                    If System.IO.File.Exists(TEMPLATE_NAME) = True Then
+
+                        'Creates an object to read the data from the template
+                        Dim dabaReader As New System.IO.StreamReader(TEMPLATE_NAME)
+                        'Creates an object to write to the newly created .dxf with overwrite ON
+                        Dim dabaWriter As New System.IO.StreamWriter(OUTFILE_NAME, True)
+
+                        'Checking to see whether there is another line; returns a 
+                        Do While dabaReader.Peek() <> -1
+                            'Clears TextLine of the last row it was storing
+                            TextLine = ""
+                            'Sets TextLine to the current row from the dxf file
+                            TextLine = dabaReader.ReadLine()
+
+                            'Checking to see if the current line is a "##" statement
+                            If TextLine.Contains("##") Then
+                                'Writes to the output file based on the current line. The current line is a ## value
+                                'and will match up to a column in the DB
+                                dabaWriter.WriteLine(dabaRow(TextLine.ToString))
+                            Else
+                                'Writes the current line from the template to the output file; has no ##
+                                dabaWriter.WriteLine(TextLine)
+                            End If
+                            'Keeps looping until the dabaReader.Peek confirms there are no more rows
+                        Loop
+                        'Closes the dabaReader now that there are no more rows
+                        dabaWriter.Close()
+                        'Closes the dabaWriter now that there are no more rows
+                        dabaReader.Close()
+
+                    Else
+                        'Alerts user if the given template location is valid
+                        MsgBox("File does not Exist")
+                    End If
+                    'Ends the for-loop after it has ran that loop once for each row in the spreadsheet
+                Next
+                'Ends the if statement checking that there are more than one row
+            End If
+            '
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+End Class
